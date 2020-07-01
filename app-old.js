30e0080a497e238ace695686148276f83f6bb32d
Horacio <rherreracalzada@gmail.com>
2020-07-01 18:57:35 -0500

    primer commit

---
+++ app-old.js
@@ -0,0 +1,20 @@
+
+const http = require('http');
+
+http.createServer ( (req, res) =>{
+	
+	res.writeHead(200, {'Content-Type': 'application/json'});
+	
+	let salida = {
+		nombre: 'Raul',
+		edad: 30,
+		url: req.url
+	}
+	
+	res.write(JSON.stringify(salida));
+	//res.write('Hola mudo');
+	res.end();
+})
+.listen(8080);
+
+console.log('Escuchando el puerto 8080');
